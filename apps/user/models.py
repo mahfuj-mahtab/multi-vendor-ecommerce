0dba7fc7e2a7a210c4456faac38ea8206907dd59
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-01-22 12:47:33 +0600

    login fixed with some extra info like vendor login

--- apps/user/models.py
+++ apps/user/models.py
@@ -6,3 +6,5 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=11,null=True)
     avatar = models.ImageField(upload_to='media/upload/',null=True)
     date_of_birth = models.DateField(null=True)
+    email_verified = models.BooleanField(default=False)
+    phone_verified = models.BooleanField(default=False)
--- apps/user/views.py
+++ apps/user/views.py
@@ -2,10 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth import authenticate,login
 from .serializers import UserSerializer  # Ensure you have a UserSerializer defined
-
+from apps.vendor.models import *
 class UserRegister(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -28,10 +29,24 @@ class UserLogin(APIView):
             if user.is_staff or user.is_superuser:
                 return Response({"error": "Invalid email or password."}, status=status.HTTP_403_FORBIDDEN)
             refresh = RefreshToken.for_user(user)
+            is_vendor = False
+            vendor_details = ''
+            try:
+                vendor = Vendor.objects.get(user=user)
+                is_vendor = True 
+                # TODO : RETURN VENDOR SERIALIZER INFO  
+                # vendor_details = 
+            except Vendor.DoesNotExist:
+                pass  
+
             data = {
                 'refresh' : str(refresh),
                 'access' : str(refresh.access_token),
-                'data' : UserSerializer(user).data
+                'data' : UserSerializer(user).data,
+                'email_verified' : user.email_verified,
+                'phone_verified' : user.phone_verified,
+                'is_vendor' : is_vendor,
+                               
             }
             return Response({"message": data}, status=status.HTTP_200_OK)
         else:
